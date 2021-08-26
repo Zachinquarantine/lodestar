58d6cf4f60e37f215ce62005da9e1fd23754c618
dapplion <35266934+dapplion@users.noreply.github.com>
2021-08-26 22:06:15 +0200

    Drop flat caches
    Simplify command
    
    Fix PR issues
    
    Use .rc
    
    Add more accurate epoch perf tests
    
    Re-arrange epoch tests with a real state
    
    Fix perf type issues
    
    Ensure state is valid
    
    Review all spec tests
    
    Fix hash computations
    
    Fix processRewardsAndPenalties
    
    Fix perf titles
    
    Re-org imports
    
    Fix get state script
    
    Perf test hashing
    
    Fix altair step
    
    Review stfn caches and performance
    
    Add SLOW CODE comments and whitespace
    
    Cache effectiveBalances
    
    Process altair attestations in batch
    
    Don't keep withdrawalCredentials and pubkey in validators flat
    
    Move flat arrays to epoch process only
    
    Add exitQueue churn cache
    
    Cache chrunLimit
    
    Optimize beacon state transition
    
    Move exitQueue cached values to epochCtx
    
    Improve error message in processAttestation
    
    Add comments to processBlock functions
    
    Allow to set validators and balances
    
    Convert from struct to tree properly
    
    Silence effectiveBalances type warnings
    
    Add data representation tradeoffs
    
    Add more docs
    
    Fix type errors
    
    More fixes
    
    
    Use better state assertion in spec tests
    
    
    Re-add delete condition
    
    
    Fix type issues
    
    
    Update perf tests
    
    
    Fix lint warnings

--- packages/beacon-state-transition/src/allForks/epoch/processEffectiveBalanceUpdates.ts
+++ packages/beacon-state-transition/src/allForks/epoch/processEffectiveBalanceUpdates.ts
@@ -10,7 +10,7 @@ import {bigIntMin} from "@chainsafe/lodestar-utils";
 import {IEpochProcess, CachedBeaconState} from "../util";
 
 /**
- * Update effective balances if validator.balance has changed enough
+ * Update effective balances if validator.balance has changed enough (hysteresis)
  *
  * PERF: Cost 'proportional' to $VALIDATOR_COUNT, to iterate over all balances. Then cost is proportional to the amount
  * of validators whose effectiveBalance changed. Worst case is a massive network leak or a big slashing event which
@@ -23,23 +23,32 @@ export function processEffectiveBalanceUpdates(
   state: CachedBeaconState<allForks.BeaconState>,
   epochProcess: IEpochProcess
 ): void {
-  const {validators} = state;
+  const {validators, epochCtx} = state;
+  const {effectiveBalances} = epochCtx;
   const HYSTERESIS_INCREMENT = EFFECTIVE_BALANCE_INCREMENT / BigInt(HYSTERESIS_QUOTIENT);
   const DOWNWARD_THRESHOLD = HYSTERESIS_INCREMENT * BigInt(HYSTERESIS_DOWNWARD_MULTIPLIER);
   const UPWARD_THRESHOLD = HYSTERESIS_INCREMENT * BigInt(HYSTERESIS_UPWARD_MULTIPLIER);
 
-  // update effective balances with hysteresis
-  (epochProcess.balances ?? state.balances).forEach((balance: bigint, i: number) => {
-    const effectiveBalance = epochProcess.validators[i].effectiveBalance;
+  for (let i = 0, len = epochProcess.balancesFlat.length; i < len; i++) {
+    const balance = epochProcess.balancesFlat[i];
+    // PERF: It's faster to access to get() every single element (4ms) than to convert to regular array then loop (9ms)
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const effectiveBalance = effectiveBalances.get(i)!;
     if (
       // Too big
       effectiveBalance > balance + DOWNWARD_THRESHOLD ||
       // Too small. Check effectiveBalance < MAX_EFFECTIVE_BALANCE to prevent unnecessary updates
       (effectiveBalance < MAX_EFFECTIVE_BALANCE && effectiveBalance < balance - UPWARD_THRESHOLD)
     ) {
-      validators.update(i, {
-        effectiveBalance: bigIntMin(balance - (balance % EFFECTIVE_BALANCE_INCREMENT), MAX_EFFECTIVE_BALANCE),
-      });
+      const newEffectiveBalance = bigIntMin(balance - (balance % EFFECTIVE_BALANCE_INCREMENT), MAX_EFFECTIVE_BALANCE);
+      if (newEffectiveBalance !== effectiveBalance) {
+        // Update the state tree
+        validators[i].effectiveBalance = newEffectiveBalance;
+        // Also update the fast cached version
+        // Should happen rarely, so it's fine to update the tree
+        // TODO: Update all in batch after this loop
+        epochCtx.effectiveBalances.set(i, newEffectiveBalance);
+      }
     }
-  });
+  }
 }
